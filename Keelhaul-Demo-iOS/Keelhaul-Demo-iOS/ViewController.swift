519e7c1818865e0a3bfcbb31ce83d701d2abf7b6
Reda Lemeden <git@kaishin.haz.email>
2015-12-11 14:56:19 +0100

    Valid receipts in the demo

--- Keelhaul-Demo-iOS/Keelhaul-Demo-iOS/ViewController.swift
+++ Keelhaul-Demo-iOS/Keelhaul-Demo-iOS/ViewController.swift
@@ -3,13 +3,50 @@ import Keelhaul
 import StoreKit
 
 class ViewController: UIViewController {
+  let keelhaul = Keelhaul(token: "token")
+
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    let keelhaul = Keelhaul(token: "token")
-
     keelhaul.validateReceipt { (isValid, receiptOpt, errorOpt) in
-      print(isValid, receiptOpt, errorOpt)
+      print(isValid, errorOpt!)
+      if isValid {
+        let alertViewController = UIAlertController(title: "Existing Receipt, Bro",
+          message: "This receipt meets Jony Ive's personal approval.",
+          preferredStyle: .Alert)
+        alertViewController.addAction(UIAlertAction(title: "Great", style: .Default) { action  in
+          alertViewController.dismissViewControllerAnimated(true, completion: nil)
+          })
+        self.presentViewController(alertViewController, animated: true, completion: nil)
+      } else {
+        print("Not valid")
+        let request = SKReceiptRefreshRequest()
+        request.delegate = self
+        request.start()
+      }
     }
+  }
+}
+
+extension ViewController: SKRequestDelegate {
+  func requestDidFinish(request: SKRequest) {
+    keelhaul.validateReceipt { (isValid, receiptOpt, errorOpt) in
+      if isValid {
+        let alertViewController = UIAlertController(title: "Valid Receipt, Bro",
+          message: "This receipt meets Jony Ive's personal approval.",
+          preferredStyle: .Alert)
+        alertViewController.addAction(UIAlertAction(title: "Great", style: .Default) { action  in
+          alertViewController.dismissViewControllerAnimated(true, completion: nil)
+          })
+        self.presentViewController(alertViewController, animated: true, completion: nil)
+      } else {
+        let alertViewController = UIAlertController(title: "Invalid Receipt, Bro",
+          message: "This receipt doesn't seem to be valid according to Tim Cook.",
+          preferredStyle: .Alert)
+        self.presentViewController(alertViewController, animated: true, completion: nil)
+      }
+    }
+  }
+  func request(request: SKRequest, didFailWithError error: NSError) {
+    print("failure! \(error.localizedDescription)")
   }
 }

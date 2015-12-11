cb2e46e69d857a21cdd762b0ea65b54bba2688e2
Reda Lemeden <git@kaishin.haz.email>
2015-12-11 11:39:18 +0100

    Parse Receipt from JSON

--- Keelhaul/Core/Keelhaul.swift
+++ Keelhaul/Core/Keelhaul.swift
@@ -1,5 +1,13 @@
- public struct Keelhaul {
+public class Keelhaul {
   private let receiptURL: NSURL?
+
+  var validationRequest: NSURLRequest {
+    return NSURLRequest()
+  }
+
+  lazy var session: NSURLSession = {
+    return NSURLSession()
+  }()
 
   init() {
     receiptURL = NSBundle.mainBundle().appStoreReceiptURL
@@ -17,5 +25,20 @@
     guard let receiptURL = receiptURL,
       let appStoreReceipt = NSData(contentsOfURL: receiptURL) else { return .None }
     return appStoreReceipt.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+  }
+
+  final func validateReceipt(completion: (Receipt?, NSError?) -> Void) {
+    session.dataTaskWithRequest(validationRequest) { data, response, error in
+      if let data = data {
+        do {
+          let json = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+          completion(Receipt.parse(json))
+        } catch let error as NSError {
+          completion(.None, error)
+        }
+      } else {
+        completion(.None, error)
+      }
+    }.resume()
   }
 }
--- Keelhaul/Core/Receipt.swift
+++ Keelhaul/Core/Receipt.swift
@@ -5,4 +5,40 @@ struct Receipt {
   let creationDate: NSDate
   let requestDate: NSDate
   let purchaseDate: NSDate
+
+  static func parse(json: AnyObject) -> (Receipt?, NSError?) {
+    guard let json = json as? [String: AnyObject],
+      let status = json["status"] as? Int else {
+        let error = NSError(domain: "com.thoughtbot.keelhaul", code: 0, userInfo: .None)
+        return (.None, error)
+    }
+
+    if status != 0 {
+      let error = NSError(domain: "com.thoughtbot.keelhaul", code: status, userInfo: .None)
+      return (.None, error)
+    }
+
+    guard let receiptDict = json["receipt"] as? [String: AnyObject],
+      let id = receiptDict["download_id"] as? Int,
+      let bundleId = receiptDict["bundle_id"] as? String,
+      let appVersion = receiptDict["application_version"] as? String,
+      let creationDateString = receiptDict["receipt_creation_date_ms"] as? String,
+      let requestDateString = receiptDict["request_date_ms"] as? String,
+      let purchaseDateString = receiptDict["original_purchase_date_ms"] as? String
+    else {
+      let error = NSError(domain: "com.thoughtbot.keelhaul", code: 0, userInfo: .None)
+      return (.None, error)
+    }
+
+    let creationDate = dateFromTimeIntervalString(creationDateString)
+    let requestDate = dateFromTimeIntervalString(requestDateString)
+    let purchaseDate = dateFromTimeIntervalString(purchaseDateString)
+
+    let receipt = Receipt(ID: id, bundleID: bundleId, appVersion: appVersion, creationDate: creationDate, requestDate: requestDate, purchaseDate: purchaseDate)
+    return (receipt, .None)
+  }
+}
+
+private func dateFromTimeIntervalString(string: String) -> NSDate {
+  return NSDate(timeIntervalSince1970:NSTimeInterval(Int(string) ?? 0))
 }

bb9acb40d3059128466f744d8a0e78d9db0839d6
Reda Lemeden <git@kaishin.haz.email>
2016-03-08 11:23:13 +0100

    Replace server API and path with placeholders

--- KeelhaulDemo/ViewController.swift
+++ KeelhaulDemo/ViewController.swift
@@ -4,9 +4,9 @@ import Keelhaul
 class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    let token = "5RcTeo2fsmkN6wFhdfiaLg"
+    let token = "<#API Key#>"
     let receiptURL = NSBundle.mainBundle().URLForResource("receipt", withExtension: nil)!
-    let endpointURL = NSURL(string: "http://localhost:3000/api/v1/verifications?sandbox=1")!
+    let endpointURL = NSURL(string: "http://<#Server Root URL#>/api/v1/verifications?sandbox=1")!
     let keelhaul = Keelhaul.init(token: token, receiptURL: receiptURL, endpointURL: endpointURL)
     keelhaul.validateReceipt { isValid, receipt, error in
       print("isValid=\(isValid)")

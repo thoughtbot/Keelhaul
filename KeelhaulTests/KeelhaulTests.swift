b725faa8bf84513abd97bc5802986c2e4b329849
Reda Lemeden <git@kaishin.haz.email>
2016-03-16 13:35:09 +0100

    Add tests for failure scenarios

--- KeelhaulTests/KeelhaulTests.swift
+++ KeelhaulTests/KeelhaulTests.swift
@@ -5,10 +5,10 @@ class KeelhaulTests: XCTestCase {
   let validReceiptURL = NSBundle(forClass: KeelhaulTests.self).URLForResource("validReceipt", withExtension: "txt")!
 
   func testValidReceipt() {
-    let expectation = expectationWithDescription("validateReceipt")
+    let expectation = expectationWithDescription("ValidateReceiptTest")
     let keelhaul = Keelhaul(token: "secret",
       receiptURL: validReceiptURL,
-      endpointURL: NSURL(string: "http://localhost:1234?success=1")!)
+      endpointURL: NSURL(string: "http://localhost:1234")!)
 
     keelhaul.validateReceipt { success, receipt, error in
       XCTAssertTrue(success)
@@ -22,6 +22,60 @@ class KeelhaulTests: XCTestCase {
         XCTAssertEqual(receipt.creationDate, dateWith(year: 2015, month: 10, day: 21, hour: 14, minute: 22, second: 48))
         XCTAssertEqual(receipt.requestDate, dateWith(year: 2015, month: 12, day: 5, hour: 15, minute: 54, second: 24))
       }
+
+      expectation.fulfill()
+    }
+
+    waitForExpectationsWithTimeout(1.0, handler: nil)
+  }
+
+  func testMismatchingEnvironment() {
+    let expectation = expectationWithDescription("MismatchingEnvTest")
+    let keelhaul = Keelhaul(token: "secret",
+      receiptURL: validReceiptURL,
+      endpointURL: NSURL(string: "http://localhost:1234/mismatching-env")!)
+
+    keelhaul.validateReceipt { success, receipt, error in
+      XCTAssertFalse(success)
+      XCTAssertNil(receipt)
+      XCTAssertNotNil(error)
+      XCTAssertEqual(error!.code, 9007)
+
+      expectation.fulfill()
+    }
+
+    waitForExpectationsWithTimeout(1.0, handler: nil)
+  }
+
+  func testAuthFailure() {
+    let expectation = expectationWithDescription("AuthFailureTest")
+    let keelhaul = Keelhaul(token: "secret",
+      receiptURL: validReceiptURL,
+      endpointURL: NSURL(string: "http://localhost:1234/auth-failure")!)
+
+    keelhaul.validateReceipt { success, receipt, error in
+      XCTAssertFalse(success)
+      XCTAssertNil(receipt)
+      XCTAssertNotNil(error)
+      XCTAssertEqual(error!.code, 9001)
+
+      expectation.fulfill()
+    }
+
+    waitForExpectationsWithTimeout(1.0, handler: nil)
+  }
+
+  func testMismatchingDevice() {
+    let expectation = expectationWithDescription("MismatchingDeviceTest")
+    let keelhaul = Keelhaul(token: "secret",
+      receiptURL: validReceiptURL,
+      endpointURL: NSURL(string: "http://localhost:1234/mismatching-device")!)
+
+    keelhaul.validateReceipt { success, receipt, error in
+      XCTAssertFalse(success)
+      XCTAssertNil(receipt)
+      XCTAssertNotNil(error)
+      XCTAssertEqual(error!.code, 9006)
 
       expectation.fulfill()
     }
--- fake_server.rb
+++ fake_server.rb
@@ -5,17 +5,29 @@ server.mount_proc "/" do |req, res|
   if req.request_method == "POST"
     res.body = <<-JSON
 {
-    "download_id": 123,
-    "bundle_id": "com.thoughtbot.Keelhaul",
-    "application_version": "1.0.0",
-    "receipt_creation_date_ms": "1445437368000",
-    "request_date_ms": "1449330864600",
-    "original_purchase_date_ms": "1425375064000",
+  "download_id": 123,
+  "bundle_id": "com.thoughtbot.Keelhaul",
+  "application_version": "1.0.0",
+  "receipt_creation_date_ms": "1445437368000",
+  "request_date_ms": "1449330864600",
+  "original_purchase_date_ms": "1425375064000",
 }
-    JSON
+  JSON
   else
     res.status = 404
   end
+end
+
+server.mount_proc "/mismatching-env" do |req, res|
+  res.status = 400
+end
+
+server.mount_proc "/auth-failure" do |req, res|
+  res.status = 401
+end
+
+server.mount_proc "/mismatching-device" do |req, res|
+  res.status = 403
 end
 
 ["INT", "TERM"].each do |signal|
